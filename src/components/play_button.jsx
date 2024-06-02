7b5d28f925cd364d2433453d13c87b64cb2260a6
Gretel Martinez <23240307+grety22@users.noreply.github.com>
2024-06-02 12:50:12 -0400

    lol - Play Button - Alter width

--- src/components/play_button.jsx
+++ src/components/play_button.jsx
@@ -3,8 +3,9 @@ import BtnNormal from "./btn_normal"
 
 export default function play_button() {
   return (
-    <>
+    // change btn width below, by using tailwind w classes or custom css 
+    <div className="btn-wrapper w-96">
       <BtnNormal label="CONFIRM" />      
-    </>
+    </div>
   )
 }
